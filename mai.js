a627c9ae65153046c86d0fae18e5213cfa8552cb
Aniket0015 <aniketto63@gmail.com>
2024-06-06 18:57:27 +0530

    deploy

---
+++ mai.js
@@ -0,0 +1,29 @@
+const endDate= "27 June 2024 0:00 am";
+
+const input = document.querySelectorAll("input");
+
+document.getElementById("enddate").innerHTML=endDate;
+function clock(){
+    const end =new Date(endDate);
+    const now= new Date();
+    const diff= (end-now)/1000;
+    if(diff<0){
+        return;
+    }
+    input[0].value= Math.floor( diff/3600/24);
+    input[1].value= Math.floor( (diff/3600)%24);
+    input[2].value= Math.floor( (diff/1440)%24);
+    input[3].value= Math.floor( (diff)%60);
+
+     
+    
+   
+    
+}
+
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
